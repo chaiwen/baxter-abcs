33a295c6028110d93390b77f6927fbbcbdbde77a
Kaixi Wu <kw2503@columbia.edu>
2016-03-29 22:27:29 -0400

    added speech command package w hello command

---
+++ src/speech_command/scripts/speech_command.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def send_command():
+    pub = rospy.Publisher('speech_command', String, queue_size=10)
+    rospy.init_node('speech_command', anonymous-True)
+    hello_str = "Hello"
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+
+if __name__ = '__main__':
+    try:
+        send_command()
+    except rospy.ROSInterruptException:
+        pass
