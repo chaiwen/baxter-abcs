8283f1e4c61be20cef62aa499d1600c3435c0f96
Kaixi Wu <kw2503@columbia.edu>
2016-03-29 22:41:10 -0400

    fixed typos in speech command script

--- src/speech_command/scripts/speech_command.py
+++ src/speech_command/scripts/speech_command.py
@@ -5,12 +5,12 @@ from std_msgs.msg import String
 
 def send_command():
     pub = rospy.Publisher('speech_command', String, queue_size=10)
-    rospy.init_node('speech_command', anonymous-True)
+    rospy.init_node('speech_command', anonymous=True)
     hello_str = "Hello"
     rospy.loginfo(hello_str)
     pub.publish(hello_str)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     try:
         send_command()
     except rospy.ROSInterruptException:
