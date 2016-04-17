a9919fa10e97d9f40ef2ab211c0402ee419e5672
Chaiwen Chou <cc3636@columbia.edu>
2016-04-16 23:22:02 -0400

    added opencv viewing window of kinect image data

--- src/vision/src/kinect_processor.cpp
+++ src/vision/src/kinect_processor.cpp
@@ -6,42 +6,64 @@
 
 #include "std_msgs/String.h"
 
-using namespace std;
+#include <sensor_msgs/PointCloud.h> // for the kinect point cloud
 
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+using namespace std;
 
 std::string turtle_name;
 
-void kinectCallback(const turtlesim::PoseConstPtr& msg)
+void kinectCallback(const sensor_msgs::ImageConstPtr& msg)
 {
     //ROS_INFO("I heard: [%s]", msg->data.c_str());
 
     static tf::TransformBroadcaster br;
 
-    tf::Transform transform;
+    /*tf::Transform transform;
     transform.setOrigin( tf::Vector3(msg->x, msg->y, 0.0) );
     tf::Quaternion q;
     q.setRPY(0, 0, msg->theta);
 
     transform.setRotation(q);
-
+*/
     // send transform, timestamp, parent frame, child frame
-    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
+    //br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
+
+    try
+    {
+        cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+        cv::waitKey(30);
+    }
+    catch(cv_bridge::Exception& e)
+    {
+        ROS_ERROR("could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    }
 }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "my_tf_broadcaster");
-    if (argc != 2){ROS_ERROR("need turtle name as argument"); return -1;};
-    turtle_name = argv[1];
+    //ros::init(argc, argv, "my_tf_broadcaster");
+    ros::init(argc, argv, "kinect_listener");
+
+    //if (argc != 2){ROS_ERROR("need turtle name as argument"); return -1;};
+    //turtle_name = argv[1];
 
     ros::NodeHandle node;  // access to ROS system
 
+    cv::namedWindow("view");
+    cv::startWindowThread();
+    image_transport::ImageTransport it(node);
+    image_transport::Subscriber sub = it.subscribe("/kinect_mount/kinect_mount/rgb/image_raw", 1, kinectCallback);
     // TODO: find correct kinect topic
-    ros::Subscriber sub = node.subscribe(turtle_name+"/pose", 10, &kinectCallback);
+    //ros::Subscriber sub = node.subscribe(turtle_name+"/pose", 10, &kinectCallback);
 
     cout << "kinect????" << endl;
-
+    
     ros::spin();
+    cv::destroyWindow("view");
 
     return 0;
 }
