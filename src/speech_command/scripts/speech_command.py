f75f8cdcbd906cf555460fe4aba4f73f3a76d370
Kaixi Wu <kw2503@columbia.edu>
2016-04-21 23:10:54 -0400

    trying to switch to mic

--- src/speech_command/scripts/speech_command.py
+++ src/speech_command/scripts/speech_command.py
@@ -4,12 +4,17 @@ import rospy
 from std_msgs.msg import String
 import speech_recognition as sr
 
-from os import path
-audio_file = path.join(path.dirname(path.realpath(__file__)), "pick_up_a_take2.wav")
+#from os import path
+#audio_file = path.join(path.dirname(path.realpath(__file__)), "pick_up_a_take2.wav")
+
+#recognizer = sr.Recognizer()
+#with sr.AudioFile(audio_file) as source:
+#    audio = recognizer.record(source)
 
 recognizer = sr.Recognizer()
-with sr.AudioFile(audio_file) as source:
-    audio = recognizer.record(source)
+with sr.Microphone() as source:
+   print "Give the robot a letter block to pick up!"
+   audio = recognizer.listen(source)
 
 try:
     # for testing purposes, we're just using the default API key
@@ -20,11 +25,6 @@ except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-#recognizer = sr.Recognizer()
-#with sr.Microphone() as source:
-#   print "Give the robot a letter block to pick up!"
-#   audio = recognizer.listen(source)
 
 def send_command():
     pub = rospy.Publisher('speech_command', String, queue_size=10)
