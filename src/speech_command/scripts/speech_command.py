fb85ef484034b4ffe8642d989d5287442d5a0885
Kaixi Wu <kw2503@columbia.edu>
2016-04-21 21:59:13 -0400

    speech from audio file

--- src/speech_command/scripts/speech_command.py
+++ src/speech_command/scripts/speech_command.py
@@ -2,6 +2,29 @@
 
 import rospy
 from std_msgs.msg import String
+import speech_recognition as sr
+
+from os import path
+audio_file = path.join(path.dirname(path.realpath(__file__)), "pick_up_a_take2.wav")
+
+recognizer = sr.Recognizer()
+with sr.AudioFile(audio_file) as source:
+    audio = recognizer.record(source)
+
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+#recognizer = sr.Recognizer()
+#with sr.Microphone() as source:
+#   print "Give the robot a letter block to pick up!"
+#   audio = recognizer.listen(source)
 
 def send_command():
     pub = rospy.Publisher('speech_command', String, queue_size=10)
