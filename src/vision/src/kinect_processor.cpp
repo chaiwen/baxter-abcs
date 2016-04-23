429c4bc68f4693f8b1ac82ae30866729b1d9fa21
Chaiwen Chou <cc3636@columbia.edu>
2016-04-23 19:21:15 -0400

    added publisher to publish kinect filtered cloud data

--- src/vision/src/kinect_processor.cpp
+++ src/vision/src/kinect_processor.cpp
@@ -14,6 +14,7 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/cloud_viewer.h>
 
 #include <image_transport/image_transport.h>
 #include <opencv2/core/core.hpp>
@@ -33,6 +34,8 @@ vector<char> corresponding(3); //indices indicate the letter that the template i
 
 vector<BlockABC *> blocks;
 
+ros::Publisher pub;
+int pclCount = 0;
 void ptCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
 
@@ -56,8 +59,10 @@ void ptCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     sensor_msgs::PointCloud2 output;
     pcl_conversions::fromPCL(cloud_filtered, output);
 
-    //output is the filtered data?
+    // publish the filtered data
+    pub.publish(output);
 
+    
 }
 
 void kinectCallback(const sensor_msgs::ImageConstPtr& msg)
@@ -67,54 +72,54 @@ void kinectCallback(const sensor_msgs::ImageConstPtr& msg)
     static tf::TransformBroadcaster br;
 
     /*tf::Transform transform;
-    transform.setOrigin( tf::Vector3(msg->x, msg->y, 0.0) );
-    tf::Quaternion q;
-    q.setRPY(0, 0, msg->theta);
+      transform.setOrigin( tf::Vector3(msg->x, msg->y, 0.0) );
+      tf::Quaternion q;
+      q.setRPY(0, 0, msg->theta);
 
-    transform.setRotation(q);
-*/
+      transform.setRotation(q);
+      */
     // send transform, timestamp, parent frame, child frame
     //br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
 
-		//on the first callback, create the dictionary of template locations
-		if (kcbCount == 0)
-		{
-			//use point cloud data to find location of blocks and create block objects
-			/* for (int i=0; i<blocks.size(); i++) {
-				//for each block, call bestMatch (make sure the sub-image is larger than the templates!
-				//set the type to the right letter, make sure the 2d positioning is accurate and being used  
-			} 
-			*/
-			cout << "kcbCount = 0" << endl;
-		}
-		else 
-		{
-			cout << "kcbCount: ";
-			cout << kcbCount << endl; 	
-		}
+    //on the first callback, create the dictionary of template locations
+    if (kcbCount == 0)
+    {
+        //use point cloud data to find location of blocks and create block objects
+        /* for (int i=0; i<blocks.size(); i++) {
+        //for each block, call bestMatch (make sure the sub-image is larger than the templates!
+        //set the type to the right letter, make sure the 2d positioning is accurate and being used  
+        } 
+        */
+        cout << "kcbCount = 0" << endl;
+    }
+    else 
+    {
+        cout << "kcbCount: ";
+        cout << kcbCount << endl; 	
+    }
 
-		try
-		{
-			cv::Mat tempImg, grayImg;
-			tempImg = cv_bridge::toCvCopy(msg, "bgr8")->image;
-			//cv::cvtColor(tempImg, grayImg, CV_BGR2GRAY);
-			//cv::imshow("view", tempImg);
+    try
+    {
+        cv::Mat tempImg, grayImg;
+        tempImg = cv_bridge::toCvCopy(msg, "bgr8")->image;
+        //cv::cvtColor(tempImg, grayImg, CV_BGR2GRAY);
+        //cv::imshow("view", tempImg);
 
-            cv::imshow("view", cv_bridge::toCvCopy(msg, "bgr8")->image);
+//        cv::imshow("view", cv_bridge::toCvCopy(msg, "bgr8")->image);
 
-			//cv::waitKey(30);
-			//writing doesn't seem to work no matter what I try... ugh. It's not too important.
-			//grayImg.convertTo(grayImg, CV_8UC3, 255.0);
-			//cv::imwrite("test.bmp", grayImg);
-/*				cout << "kinect callback count: " <<endl;
-			cout << kcbCount << endl; 
-			cout << "i only call you when it's half past five" << endl;
-*/
-		}
-		catch(cv_bridge::Exception& e)
-		{
-			ROS_ERROR("could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-		}
+        //cv::waitKey(30);
+        //writing doesn't seem to work no matter what I try... ugh. It's not too important.
+        //grayImg.convertTo(grayImg, CV_8UC3, 255.0);
+        //cv::imwrite("test.bmp", grayImg);
+        /*				cout << "kinect callback count: " <<endl;
+                        cout << kcbCount << endl; 
+                        cout << "i only call you when it's half past five" << endl;
+                        */
+    }
+    catch(cv_bridge::Exception& e)
+    {
+        ROS_ERROR("could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    }
 
     kcbCount++;
 }
@@ -123,106 +128,106 @@ void kinectCallback(const sensor_msgs::ImageConstPtr& msg)
 char bestMatch(cv::Mat cubeSnippet)
 { 
 
-	double curMaxVal;
-	int bestMatchIndex;
-	Point matchLoc; 
-	int matchMethod = CV_TM_CCORR_NORMED;
-	
-	//use each image in the template library as the template to match against cubesnippet
+    double curMaxVal;
+    int bestMatchIndex;
+    Point matchLoc; 
+    int matchMethod = CV_TM_CCORR_NORMED;
 
-	for (int i=0; i<templates.size(); i++){ 
-		cv::Mat result;
-		double minVal, maxVal; 
-		Point minLoc, maxLoc;
+    //use each image in the template library as the template to match against cubesnippet
 
-		int result_cols = cubeSnippet.cols - templates[i].cols + 1;
-		int result_rows = cubeSnippet.rows - templates[i].rows + 1; 
-/*
-		cout << "result columns" << endl;
-		cout << result_cols << endl;
-		cout << "result rows" << endl;
-		cout << result_rows << endl; 
-*/
-		result.create(result_rows, result_cols, CV_32FC1);
-		cv::matchTemplate( cubeSnippet, templates[i], result, matchMethod);
-		//cv::normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());		
-		cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-/*
-		cout << "current max value: " ;
-		cout << curMaxVal << endl; 
-		cout << "max value: " ;
-		cout << maxVal << endl;
-		cout << "min value: " ;
-		cout << minVal << endl; 
-*/
+    for (int i=0; i<templates.size(); i++){ 
+        cv::Mat result;
+        double minVal, maxVal; 
+        Point minLoc, maxLoc;
 
-		if (maxVal > curMaxVal) {
-			curMaxVal = maxVal;
-			matchLoc = maxLoc;
-			bestMatchIndex = i;
-/*
-			cout << "best match index" << endl;
-			cout << bestMatchIndex << endl;
-*/   
-		}
- 
-	}
-	return corresponding[bestMatchIndex]; 
+        int result_cols = cubeSnippet.cols - templates[i].cols + 1;
+        int result_rows = cubeSnippet.rows - templates[i].rows + 1; 
+        /*
+           cout << "result columns" << endl;
+           cout << result_cols << endl;
+           cout << "result rows" << endl;
+           cout << result_rows << endl; 
+           */
+        result.create(result_rows, result_cols, CV_32FC1);
+        cv::matchTemplate( cubeSnippet, templates[i], result, matchMethod);
+        //cv::normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());		
+        cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+        /*
+           cout << "current max value: " ;
+           cout << curMaxVal << endl; 
+           cout << "max value: " ;
+           cout << maxVal << endl;
+           cout << "min value: " ;
+           cout << minVal << endl; 
+           */
+
+        if (maxVal > curMaxVal) {
+            curMaxVal = maxVal;
+            matchLoc = maxLoc;
+            bestMatchIndex = i;
+            /*
+               cout << "best match index" << endl;
+               cout << bestMatchIndex << endl;
+               */   
+        }
+
+    }
+    return corresponding[bestMatchIndex]; 
 }
 
 int main(int argc, char **argv)
 {
-		//load images into template library
-		cv::Mat a, b, c, testb;
-		char matchResult;
+    //load images into template library
+    cv::Mat a, b, c, testb;
+    char matchResult;
     a = cv::imread("/home/yl2908/baxter-abcs/src/vision/src/templates/a.png", 1);
-		b = cv::imread("/home/yl2908/baxter-abcs/src/vision/src/templates/b.png", 1);
-		c = cv::imread("/home/yl2908/baxter-abcs/src/vision/src/templates/c.png", 1);
-		//testb = cv::imread("/home/yl2908/baxter-abcs/src/vision/src/templates/testb.png", 1);
-		templates[0] = a;
-		templates[1] = b;
-		templates[2] = c;
-		corresponding[0] = 'a';
-		corresponding[1] = 'b';
-		corresponding[2] = 'c';
+    b = cv::imread("/home/yl2908/baxter-abcs/src/vision/src/templates/b.png", 1);
+    c = cv::imread("/home/yl2908/baxter-abcs/src/vision/src/templates/c.png", 1);
+    //testb = cv::imread("/home/yl2908/baxter-abcs/src/vision/src/templates/testb.png", 1);
+    templates[0] = a;
+    templates[1] = b;
+    templates[2] = c;
+    corresponding[0] = 'a';
+    corresponding[1] = 'b';
+    corresponding[2] = 'c';
 
-/* works!
-		matchResult = bestMatch(testb); 
-		cout << "The tester match result: " << endl;
-		cout << matchResult << endl; 
-*/
+    /* works!
+       matchResult = bestMatch(testb); 
+       cout << "The tester match result: " << endl;
+       cout << matchResult << endl; 
+       */
 
-		cout << kcbCount << endl; 
-		/*
-		string alpha ("abcdefghijklmnopqrstuvwxyz");
-    string ext(".png"); 
-		string name;
-		DIR* dirp = null;
-		struct dirent* dp = null;
-		dirp = opendir(".");
-		if (dirp == NULL) {
-			cout << "yea you fucked up" ;
-		}
-		else {
-			dp = readdir(dirp);
-			cout << "\nDirectory name: " ;
-			cout << dp->d_name ;
-		}    
-		for (int i=0; i<alpha.length(); ++i)
-		{    
-			name = alpha[i] + ext;
-			dirp = opendir("templates");
-			while ((dp = readdir(dirp)) != NULL) {
-				//if (strcmp(dp->d_name,name.c_str()) == 0) { 
-						//(void) closedir(dirp);
-            //cout << "\nfound: " + name ;
-				//}
-				cout << dp->d_name;	
-			}
-			(void) closedir(dirp);
-			cout << "\nnot found: " + name;
- 		}
-		*/
+    cout << kcbCount << endl; 
+    /*
+       string alpha ("abcdefghijklmnopqrstuvwxyz");
+       string ext(".png"); 
+       string name;
+       DIR* dirp = null;
+       struct dirent* dp = null;
+       dirp = opendir(".");
+       if (dirp == NULL) {
+       cout << "yea you fucked up" ;
+       }
+       else {
+       dp = readdir(dirp);
+       cout << "\nDirectory name: " ;
+       cout << dp->d_name ;
+       }    
+       for (int i=0; i<alpha.length(); ++i)
+       {    
+       name = alpha[i] + ext;
+       dirp = opendir("templates");
+       while ((dp = readdir(dirp)) != NULL) {
+    //if (strcmp(dp->d_name,name.c_str()) == 0) { 
+    //(void) closedir(dirp);
+    //cout << "\nfound: " + name ;
+    //}
+    cout << dp->d_name;	
+    }
+    (void) closedir(dirp);
+    cout << "\nnot found: " + name;
+    }
+    */
     //ros::init(argc, argv, "my_tf_broadcaster");
     ros::init(argc, argv, "kinect_listener");
 
@@ -240,8 +245,11 @@ int main(int argc, char **argv)
 
     ros::Subscriber sub2 = node.subscribe<sensor_msgs::PointCloud2>("/kinect_mount/kinect_mount/rgb/points", 1, ptCloudCallback);
 
+
+    // publisher for modified cloud
+    pub = node.advertise<sensor_msgs::PointCloud2>("/kinect_mount/kinect_mount/kinect_object_cloud_filtered", 100);
     cout << "kinect????" << endl;
-    
+
     BlockABC testA('a');
 
     ros::spin();
