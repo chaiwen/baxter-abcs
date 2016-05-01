b5fade07ab40733884d8c7329719839d3e8025a4
Kaixi Wu <kw2503@columbia.edu>
2016-05-01 00:49:34 -0400

    listen foreverrrr until we say stop

--- src/speech_command/scripts/speech_command.py
+++ src/speech_command/scripts/speech_command.py
@@ -4,53 +4,60 @@ import rospy
 from std_msgs.msg import String
 import speech_recognition as sr
 
+# init node and publisher
+
 rospy.init_node('speech_command')
 pub = rospy.Publisher('speech_command', String, queue_size=10)
 
-#uncomment the following to use speech from audio file:
+def listen_for_command():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+       print "\nGive the robot a letter block to pick up!"
+       audio = recognizer.listen(source)
 
-#from os import path
-#audio_file = path.join(path.dirname(path.realpath(__file__)), "pick_up_a_take2.wav")
+    audio_text = ""
 
-#recognizer = sr.Recognizer()
-#with sr.AudioFile(audio_file) as source:
-#    audio = recognizer.record(source)
+    try:
+        # we are using the default API key. to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        audio_text = recognizer.recognize_google(audio)
+    except sr.UnknownValueError:
+        pass
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-recognizer = sr.Recognizer()
-with sr.Microphone() as source:
-   print "\nGive the robot a letter block to pick up!"
-   audio = recognizer.listen(source)
+    return audio_text
 
-audio_text = ""
+def parse_command(command_text):
+    block_letters = ""
 
-try:
-    # we are using the default API key. to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    audio_text = recognizer.recognize_google(audio)
-    print("\nThe command you gave was: \n" + audio_text + "\n")
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    words = command_text.split()
+    if "spell" in words or "Spell" in words:
+        block_letters = words[-1]
+    else:
+        for word in words[1:]:
+            if len(word) == 1:
+                block_letters = word
 
-block_letters = ""
+    block_letters = block_letters.upper()
 
-words = audio_text.split()
-if "spell" in words or "Spell" in words:
-    block_letters = words[-1]
-else:
-    for word in words[1:]:
-        if len(word) == 1:
-            block_letters = word
+    return block_letters
 
-block_letters = block_letters.upper()
-
-def send_command():
-    #block_letters = "A"
-    rospy.loginfo(block_letters)
+def send_command(block_letters):
+    rospy.loginfo(block_letters + "\n")
     pub.publish(block_letters)
 
-if __name__ == '__main__':
-    try:
-        send_command()
-    except rospy.ROSInterruptException:
-        pass
+if __name__ == '__main__': 
+    continue_listening = True
+    while (continue_listening):
+        try:
+            command_text = listen_for_command()
+            if command_text.upper() == "NO" or command_text.upper() == "STOP":
+                continue_listening = False
+                break
+            if command_text != "":
+                print("\nThe command you gave was: \n" + command_text + "\n")
+                block_letters = parse_command(command_text)
+                send_command(block_letters)
+        except rospy.ROSInterruptException:
+            pass
+
