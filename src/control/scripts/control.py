346815195afa0f32f0519b87bafaef692c6d431c
Yumeng Liao <yl2908@columbia.edu>
2016-05-01 02:11:28 -0400

    changed the code so the only thing we need to add is finish up the vision stuff and do gripping

--- src/control/scripts/control.py
+++ src/control/scripts/control.py
@@ -11,7 +11,8 @@ rospy.init_node('control', anonymous=False)
 limb = baxter_interface.Limb('right')
 
 angles = {}
-
+blocks = []
+init_placement = (5,5,100)
 
 
 # client to get XYZ from letter of block alphabet
@@ -25,10 +26,37 @@ def get_xyz_client(abc):
         print "Service call failed: %s"%e
 
 
+# this is where we will separate the thing to be spelled into a list of locations, to be picked up in that order
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    wave()
+#    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+#    wave()
+		separated = list(data.data)
+		print separated
+		for l in separated:
+				tup = get_letter_position(l) 
+				#print 'callback tuple: '
+				#print tup
+				blocks.append(tup) 
 
+		# print "the final blocks in order" 
+		# print blocks
+
+		for block in blocks:
+			print block
+			place_block(block)
+			
+
+# 3 item tuple in format ( x, y, z)
+def get_letter_position(letter):
+		print 'Requesting position of: ' + letter + '\n'
+		x, y, z = get_xyz_client(letter)
+		return (x,y,z) 
+
+
+def place_block(block_tuple):
+		# (x, y, z) 
+		#
+		print "yea baxter is moving the block with initial location: x=" + str(block_tuple[0]) + " y=" + str(block_tuple[1]) + " z=" + str(block_tuple[2])
 
 def wave():
     # store first and second wave position
@@ -65,11 +93,10 @@ def control():
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
+
 if __name__ == '__main__':
-    #control() # this will subscribe and wave robot arm?
-    abc = "BLOCK A!"
-    print "Requesting position of %s"%(abc)
-
-    x, y, z = get_xyz_client(abc)
-
-    print "%s = %d %d %d"%(abc, x, y, z)
+    control() # this will subscribe and wave robot arm?
+    
+		#abc = "BLOCK A!"
+    #print "Requesting position of %s"%(abc)
+    #print "%s = %d %d %d"%(abc, x, y, z)
--- src/speech_command/scripts/speech_command.py
+++ src/speech_command/scripts/speech_command.py
@@ -6,8 +6,8 @@ from std_msgs.msg import String
 def send_command():
     pub = rospy.Publisher('speech_command', String, queue_size=10)
     rospy.init_node('speech_command', anonymous=True)
-    block_letters = "A"
-    #block_letters = "CAB"
+    #block_letters = "A"
+    block_letters = "FALALFEL"
     rospy.loginfo(block_letters)
     pub.publish(block_letters)
 
