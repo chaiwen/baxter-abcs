1d75ff7f1b2c7b4926d7ee0f5deb3ac28bc7ab54
Kaixi Wu <kw2503@columbia.edu>
2016-04-30 23:57:16 -0400

    speech rec from mic

--- src/speech_command/scripts/speech_command.py
+++ src/speech_command/scripts/speech_command.py
@@ -4,6 +4,11 @@ import rospy
 from std_msgs.msg import String
 import speech_recognition as sr
 
+rospy.init_node('speech_command')
+pub = rospy.Publisher('speech_command', String, queue_size=10)
+
+#uncomment the following to use speech from audio file:
+
 #from os import path
 #audio_file = path.join(path.dirname(path.realpath(__file__)), "pick_up_a_take2.wav")
 
@@ -13,24 +18,34 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer()
 with sr.Microphone() as source:
-   print "Give the robot a letter block to pick up!"
+   print "\nGive the robot a letter block to pick up!"
    audio = recognizer.listen(source)
 
+audio_text = ""
+
 try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
+    # we are using the default API key. to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    audio_text = recognizer.recognize_google(audio)
+    print("\nThe command you gave was: \n" + audio_text + "\n")
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
+block_letters = ""
+
+words = audio_text.split()
+if "spell" in words or "Spell" in words:
+    block_letters = words[-1]
+else:
+    for word in words[1:]:
+        if len(word) == 1:
+            block_letters = word
+
+block_letters = block_letters.upper()
+
 def send_command():
-    pub = rospy.Publisher('speech_command', String, queue_size=10)
-    rospy.init_node('speech_command', anonymous=True)
-    block_letters = "A"
-    #block_letters = "CAB"
+    #block_letters = "A"
     rospy.loginfo(block_letters)
     pub.publish(block_letters)
 
